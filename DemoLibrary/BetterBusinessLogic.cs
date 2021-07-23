0450b69623bcdf2dc2dd854b10a4aaca30237513
Erutase Akpobome <erutase.akpobome@liverpoolft.nhs.uk>
2021-07-23 11:08:14 +0100

    Add project files.

---
+++ DemoLibrary/BetterBusinessLogic.cs
@@ -0,0 +1,32 @@
+﻿using DemoLibrary.Utilities;
+using System;
+
+namespace DemoLibrary
+{
+    public class BetterBusinessLogic : IBusinessLogic
+    {
+
+        private readonly ILogger _logger;
+        private readonly IDataAccess _dataAccess;
+
+        public BetterBusinessLogic(ILogger logger, IDataAccess dataAccess)
+        {
+            _logger = logger;
+            _dataAccess = dataAccess;
+        }
+        public void ProcessData()
+        {
+
+
+            _logger.Log("Starting the processing of data.");
+            Console.WriteLine();
+            Console.WriteLine("Processing the data");
+            Console.WriteLine();
+            _dataAccess.LoadData();
+            Console.WriteLine();
+            _dataAccess.SaveData("ProcessedInfo");
+            Console.WriteLine();
+            _logger.Log("Finished processing of the data.");
+        }
+    }
+}
